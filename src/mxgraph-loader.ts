f17d35b9054a69d674a4205d791f646c5dddf3a7
Thomas Bouffard <27200110+tbouffard@users.noreply.github.com>
2022-05-07 07:57:19 +0200

    refactor: improve mxGraph initialization
    Avoid `any` when possible and better explain why we use `globals` instead of mxGraph options.

--- src/mxgraph-loader.ts
+++ src/mxgraph-loader.ts
@@ -2,12 +2,24 @@ import factory, {type mxGraphExportObject} from 'mxgraph';
 
 export default initialize();
 
+// mxGraph globals
+declare global {
+  interface Window {
+    mxForceIncludes: boolean;
+    mxLoadResources: boolean;
+    mxLoadStylesheets: boolean;
+    mxResourceExtension: string;
+  }
+}
+
 function initialize(): mxGraphExportObject {
     // set options globally, as it is not working when passing options to the factory (https://github.com/jgraph/mxgraph/issues/479)
-    (window as any)['mxLoadResources'] = false;
-    (window as any)['mxLoadStylesheets'] = false;
-    // extras, otherwise we got 'Uncaught ReferenceError: assignment to undeclared variable mxForceIncludes'
-    (window as any)['mxForceIncludes'] = false;
-    (window as any)['mxResourceExtension'] = '.txt';
+    // Required otherwise 'Uncaught ReferenceError: assignment to undeclared variable mx...'
+    window.mxForceIncludes = false;
+    window.mxLoadResources = false;
+    // Required otherwise we got 'Uncaught ReferenceError: assignment to undeclared variable mx...'
+    window.mxLoadStylesheets = false;
+    window.mxResourceExtension = '.txt';
+
     return factory({});
 }
